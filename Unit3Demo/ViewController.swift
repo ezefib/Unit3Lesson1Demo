ad41eaccd3f2d0d2ea9272bc2e589c8674a756a9
Eric Sarmiento <ericsarmiento@Eric-Sarmientos-iMac.local>
2014-09-10 22:34:35 -0700

    initial commit

---
+++ Unit3Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit3Demo
+//
+//  Created by Eric Sarmiento on 9/8/14.
+//  Copyright (c) 2014 Eric Sarmiento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Unit3DemoTests/Unit3DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Unit3DemoTests.swift
+//  Unit3DemoTests
+//
+//  Created by Eric Sarmiento on 9/8/14.
+//  Copyright (c) 2014 Eric Sarmiento. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Unit3DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
